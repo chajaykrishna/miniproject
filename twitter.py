166743863a486aeffa1b47a8f7d2efc5ac2c8312
chajaykrishna <chajay55555@gmail.com>
2018-09-10 01:49:47 +0530

    max count changed

--- twitter.py
+++ twitter.py
@@ -23,7 +23,7 @@ class TwitterClnt(object):
             self.retweets_only = retweets_only
             self.with_sentiment = with_sentiment
             self.api = tweepy.API(self.auth)
-            self.tweet_count_max = 100  # To prevent Rate Limiting
+            self.tweet_count_max = 50  # To prevent Rate Limiting
         except:
             print("Error: Authentication Failed")
 
